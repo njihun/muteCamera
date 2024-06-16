be5edf49a3c9c5f5b429f4e18908edcdd9e81d58
njihun <njihun@kakao.com>
2024-06-16 14:53:12 +0900

    .

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let video;
 let canvasElement;
 let container = document.querySelector('#container');
-innerSize = [window.innerWidth, window.innerHeight];
+let innerSize = [window.innerWidth, window.innerHeight];
 function openCamera() {
+    innerSize = [window.innerWidth, window.innerHeight];
     canvasElement = document.createElement('canvas');
     let canvas = canvasElement.getContext('2d');
     container.innerHTML = '';
@@ -11,50 +12,44 @@ function openCamera() {
 
     let div = document.createElement('div');
     div.style.width = `${innerSize[0]}px`;
-    div.style.height = `${innerSize[1]*0.88 - 200}px`;
+    div.style.height = `${innerSize[1]*0.88 - innerSize[1]*0.3}px`;
+    div.style.display = 'flex';
+    div.style.alignItems = 'center';
+    div.style.justifyContent = 'center';
     div.appendChild(canvasElement);
     container.appendChild(div);
     
     div = document.createElement('div');
     div.style.width = `${innerSize[0]}px`;
-    div.style.height = `${200}px`;
+    div.style.height = `${innerSize[1]*0.3}px`;
     
     let div2 = document.createElement('div');
     div2.style.display = 'flex';
     div2.style.justifyContent = 'center';
 
     let cameraType = document.createElement('div');
-    cameraType.style.fontSize = '15px';
-    cameraType.style.fontWeight = 'bold';
-    cameraType.style.padding = '10px';
-    cameraType.style.paddingLeft = '20px';
-    cameraType.style.paddingRight = '20px';
-    cameraType.style.margin = '10px';
-    cameraType.style.borderRadius = '50px';
-    cameraType.style.backgroundColor = 'lightgray';
+    cameraType.style.fontSize = '2vh';
+    cameraType.style.padding = '1.2vh';
+    cameraType.style.paddingLeft = '2.5vh';
+    cameraType.style.paddingRight = '2.5vh';
+    cameraType.style.margin = '1.5vh';
+    cameraType.style.borderRadius = '90px';
     cameraType.textContent = '사진';
-    // cameraType.style.margin = '0 auto';
-    // cameraType.style.transform = 'translateX(-50%)';
-    // cameraType.style.left = '50%';
-    // cameraType.style.position = 'absolute';
 
     div2.appendChild(cameraType);
     
     cameraType = cameraType.cloneNode(cameraType);
-    // cameraType.style.backgroundColor = 'unset';
     cameraType.textContent = '동영상';
+    div2.appendChild(cameraType);
+    
+    cameraType = cameraType.cloneNode(cameraType);
+    cameraType.textContent = '더보기';
     div2.appendChild(cameraType);
     
     div.appendChild(div2);
     container.appendChild(div);
-    let x = 0;
-    let y = 0;
-    if(x==0){
-        y = -20-(document.querySelector('#container > div > div:nth-child(1) > div:nth-child(2)').getBoundingClientRect().width)/2
-    }
-    div = document.querySelector('#container > div > div:nth-child(1)');
-    div.style.transform = `translateX(${(document.querySelector('#container > div > div:nth-child(1) > div:nth-child(2)').getBoundingClientRect().width - document.querySelector('#container > div > div:nth-child(1) > div:nth-child(1)').getBoundingClientRect().width)/2}px)`;
-    // div.style.transform = `translateX(${y}px)`;
+
+    selectType();
 
     video = document.createElement('video');
     navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user'/*environment*/ } }).then(function (stream) {
@@ -65,16 +60,37 @@ function openCamera() {
     });
     function tick() {
         if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            canvasElement.width = innerSize[0];
-            canvasElement.height = innerSize[1]*0.48;
-            // canvasElement.width = video.videoWidth;
-            // canvasElement.height = video.videoHeight;
-            // canvasElement.width = video.videoWidth;
-            // canvasElement.height = video.videoHeight;
+            // canvasElement.width = innerSize[0];
+            // canvasElement.height = innerSize[1]*0.48;
+            canvasElement.width = video.videoWidth;
+            canvasElement.height = video.videoHeight;
             canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
         }
         requestAnimationFrame(tick);
     }
+}
+function selectType(x=0) {
+    let types = document.querySelectorAll('#container > div > div:nth-child(1) > *')
+    let typesWidth = [0, 0];
+    types.forEach((e, i)=>{
+        e.onclick = () => selectType(i);
+        e.style.fontWeight = 'unset';
+        e.style.backgroundColor = 'unset';
+        if(x<i){
+            typesWidth[0] += e.getBoundingClientRect().width;
+            typesWidth[0] += innerSize[1]*0.03;//cameraType.style.margin
+        }else if(x>i){
+            typesWidth[1] += e.getBoundingClientRect().width;
+            typesWidth[1] += innerSize[1]*0.03;//cameraType.style.margin
+        }else{
+            e.style.fontWeight = 'bold';
+            e.style.backgroundColor = 'lightgray';
+        }
+    });
+    let y = typesWidth[1]-typesWidth[0];
+    y /= 2;
+    div = document.querySelector('#container > div > div:nth-child(1)');
+    div.style.transform = `translateX(${-y}px)`;
 }
 function closeCamera(stream = video.srcObject) {
     if (stream) {
