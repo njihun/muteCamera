fc41a89c7d0477414b89ec261dedca43919e99f5
njihun <njihun@kakao.com>
2024-06-17 15:58:43 +0900

    asfd

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let video;
 let canvasElement;
 let container = document.querySelector('#container');
 let innerSize = [innerWidth, innerHeight];
+let facingMode = 'user';
+let gallery = [];
 document.addEventListener('fullscreenchange', () => {
     innerSize = [innerWidth, innerHeight];
 });
@@ -78,16 +80,24 @@ function openCamera() {
     div3.style.height = '8vh';
     div3.style.backgroundColor = 'white';
     div3.style.borderRadius = '50%';
+    div3.id = 'gallery';
     div2.appendChild(div3);
 
     div3 = div3.cloneNode();
     div3.style.width = '10vh';
     div3.style.height = '10vh';
+    div3.id = 'shutter';
     div2.appendChild(div3);
 
     div3 = div3.cloneNode();
     div3.style.width = '8vh';
     div3.style.height = '8vh';
+    div3.style.lineHeight = '8vh';
+    div3.style.textAlign = 'center';
+    div3.style.color = 'black';
+    div3.onclick = () => startVideo(video, facingMode=='user'?'environment':'user');
+    div3.id = 'change';
+    div3.textContent = 'change';
     div2.appendChild(div3);
 
     div.appendChild(div2);
@@ -97,30 +107,40 @@ function openCamera() {
     selectType();
 
     video = document.createElement('video');
-    navigator.mediaDevices.getUserMedia({ video: { facingMode: 'user'/*environment*/ } }).then(function (stream) {
-        video.srcObject = stream;
-        video.setAttribute('playsinline', true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
-        video.play();
-        requestAnimationFrame(tick);
-    });
+
+    startVideo(video);
+    async function startVideo(video, facingMode2 = 'user') {
+        try {
+            facingMode = facingMode2;
+            const stream = await navigator.mediaDevices.getUserMedia({
+                video: {
+                    facingMode: { exact: facingMode2 }
+                }
+            }).then(function (stream) {
+                video.srcObject = stream;
+                video.setAttribute('playsinline', true);      // iOS 사용시 전체 화면을 사용하지 않음을 전달
+                video.play();
+                requestAnimationFrame(tick);
+            });
+        } catch (err) {
+            console.error("Error accessing the camera: ", err);
+        }
+    }
     function tick() {
         if (video.readyState === video.HAVE_ENOUGH_DATA) {
             innerSize = [innerWidth, innerHeight];
             let height = Number(document.getElementById('more').style.height.replace('vh', ''));
+            canvasElement.width = video.videoWidth/video.videoHeight*(height/100*innerSize[1]);
+            canvasElement.height = video.videoHeight/video.videoWidth*innerSize[0];
             if (video.videoWidth > video.videoHeight) {
-                canvasElement.height = video.videoHeight/video.videoWidth*innerSize[0];
                 if(canvasElement.height>height/100*innerSize[1]){
                     canvasElement.height = height/100*innerSize[1];
-                    //width 미완성
-                    // canvasElement.width = innerSize[0];
                 }else{
                     canvasElement.width = innerSize[0];
                 }
             } else {
-                canvasElement.width = video.videoWidth/video.videoHeight*(height/100*innerSize[1]);
                 if(canvasElement.width>innerSize[0]){
                     canvasElement.width = innerSize[0];
-                    //height 미완성
                 }else{
                     canvasElement.height = height/100*innerSize[1];
                 }
@@ -161,13 +181,35 @@ function selectType(x = 0) {
 
     canvasElement.style.filter = 'unset';
     document.getElementById('more').style.display = 'none';
+    document.getElementById('shutter').onclick = () => null;
+    document.querySelector("#container > div:nth-child(3) > div:nth-child(2)").style.display = 'none';
     switch (x) {
         case 0:
+            document.querySelector("#container > div:nth-child(3) > div:nth-child(2)").style.display = 'flex';
+            document.getElementById('shutter').onclick = () => {
+                let img = new Image();
+                img.src = canvasElement.toDataURL('image/png');
+                gallery.unshift(img);
+                document.getElementById('gallery').style.backgroundImage = `url(${img.src})`;
+            };
+            document.getElementById('gallery').onclick = () => {
+                // document.querySelector("#container > div:nth-child(1)").appendChild(imageDownload(gallery[0]));
+
+            };
             break;
         case 1:
+            document.querySelector("#container > div:nth-child(3) > div:nth-child(2)").style.display = 'flex';
+            document.getElementById('shutter').onclick = () => {
+                alert('아직 지원하지 않는 기능입니다.');
+                return;
+                let img = new Image();
+                img.src = canvasElement.toDataURL('image/png');
+                gallery.unshift(img);
+                document.getElementById('gallery').style.backgroundImage = `url(${img.src})`;
+            };
             break;
         case 2:
-            canvasElement.style.filter = 'blur(50px)';
+            canvasElement.style.filter = 'blur(1vh)';
             document.getElementById('more').style.display = 'block';
             break;
         default:
@@ -192,12 +234,11 @@ function closeCamera(stream = video.srcObject) {
  * @param {String} textContent 다운로드 버튼 텍스트
  * @returns 클릭하면 사진이 다운로드되는 a태그
  */
-function imageDownload(canvas, fileName = 'image', textContent = 'Download') {
+function imageDownload(img, fileName = 'image', textContent = 'Download') {
     let a = document.createElement('a');
     a.textContent = textContent;
-    let img = canvas.toDataURL('image/png').replace("image/png", "image/octet-stream");
     a.setAttribute('download', `${fileName}.png`);
-    a.setAttribute('href', img);
+    a.setAttribute('href', img.src);
     return a;
 }
 function isFullscreen() {
